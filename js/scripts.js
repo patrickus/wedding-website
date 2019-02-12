6a48e071c800331cf6574424a068f579840318ae
patrick <patimpey@gmail.com>
2019-02-11 16:17:16 -0800

    form working

--- js/scripts.js
+++ js/scripts.js
@@ -167,20 +167,20 @@ $(document).ready(function () {
             title: "Patrick and Ali's Wedding",
 
             // Event start date
-            start: new Date('Nov 27, 2017 10:00'),
+            start: new Date('Sept 14, 2019 04:00'),
 
             // Event duration (IN MINUTES)
             // duration: 120,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
+            end: new Date('Sept 15, 2019 00:00'),
 
             // Event Address
-            address: 'ITC Fortune Park Hotel, Kolkata',
+            address: '18501 Bear Creek Rd, Los Gatos, CA 95033',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues contact Mr. Amit Roy at +91 9435021804 or +91 7086018971."
+            description: "We can't wait to see you on our big day. For any queries or issues contact Patrick at (408) 761-8374."
         }
     });
 
@@ -194,15 +194,16 @@ $(document).ready(function () {
         console.log('hereeeeeeee');
         e.stopPropagation();
         e.preventDefault();
-        var data = $(this).serialize();
+        var data = $('#rsvp-form').serialize();
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
+        console.log(data);
         if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbzlsUD1WHQWeX9Pzh9nQssnD23QUyuKJkDx_Ap11AIdun1hJOQ/exec', data)
+            $.post('https://cors-anywhere.herokuapp.com/https://script.google.com/macros/s/AKfycbzlsUD1WHQWeX9Pzh9nQssnD23QUyuKJkDx_Ap11AIdun1hJOQ/exec', data)
                 .done(function (data) {
                     console.log(data);
                     $('#alert-wrapper').html('');
