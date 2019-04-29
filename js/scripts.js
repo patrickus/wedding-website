89b2fc401ef07ca2b3f59d62ef45f5099510dd7a
patrick <patimpey@gmail.com>
2019-04-28 17:44:49 -0700

    updating wedding website

--- js/scripts.js
+++ js/scripts.js
@@ -227,22 +227,16 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        console.log(data);
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
-            $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
-        } else {
-            $.post('https://cors-anywhere.herokuapp.com/https://script.google.com/macros/s/AKfycbzlsUD1WHQWeX9Pzh9nQssnD23QUyuKJkDx_Ap11AIdun1hJOQ/exec', data)
-                .done(function (data) {
-                    console.log(data);
-                    $('#alert-wrapper').html('');
-                    $('#rsvp-modal').modal('show');
-                })
-                .fail(function (data) {
-                    console.log(data);
-                    $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
-                });
-        }
+        $.post('https://cors-anywhere.herokuapp.com/https://script.google.com/macros/s/AKfycbzlsUD1WHQWeX9Pzh9nQssnD23QUyuKJkDx_Ap11AIdun1hJOQ/exec', data)
+            .done(function (data) {
+                console.log(data);
+                $('#alert-wrapper').html('');
+                $('#rsvp-modal').modal('show');
+            })
+            .fail(function (data) {
+                console.log(data);
+                $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
+            });
 
         return false;
     });
